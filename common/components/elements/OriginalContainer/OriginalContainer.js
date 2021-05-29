eced4e5803be314d5968ab3ab40da043aa6fd067
Pedro Mata Rodrigues <me@pmatarodrigues.com>
2021-05-29 14:45:17 +0100

    feat: use app context on code containers

--- common/components/elements/OriginalContainer/OriginalContainer.js
+++ common/components/elements/OriginalContainer/OriginalContainer.js
@@ -1,13 +1,34 @@
 // REACT, STYLE, STORIES & COMPONENT
-import React from 'react';
+import React, { createContext, useContext, useReducer, useState } from 'react';
+import CodeContext from '../../../../context/state';
 import styles from './OriginalContainer.module.css';
 
 // COMPONENT: OriginalContainer
 const OriginalContainer = (props) => {
+    const { code, setCode } = useContext(CodeContext);
+
+    // Verify if any code was input into original container
+    const didUserStartTyping = () => {
+        if (code.length > 0) {
+            return true;
+        }
+        return false;
+    }
 
     return (
         <div className={styles.container}>
-            <textarea className={styles.originalInput} />
+            <textarea
+                className={styles.originalInput}
+                value={code}
+                onChange={(e) => setCode(e.target.value)}
+            />
+            {
+                didUserStartTyping() ? (
+                    <div></div>
+                ) : (
+                    <h1 className={styles.initialText}>Start typing...</h1>
+                )
+            }
         </div>
     );
 };
--- common/components/elements/ResultContainer/ResultContainer.js
+++ common/components/elements/ResultContainer/ResultContainer.js
@@ -1,14 +1,23 @@
 // REACT, STYLE, STORIES & COMPONENT
-import React from 'react';
+import React, { useContext } from 'react';
 import styles from './ResultContainer.module.css';
+import CodeContext from '../../../../context/state';
+
+const generateCode = (originalCode) => {
+    return originalCode + "------";
+}
 
 // COMPONENT: ResultContainer
 const ResultContainer = (props) => {
+    const { code, setCode } = useContext(CodeContext);
+
+    // Convert user input code into the snippet
+    let resultCode = generateCode(code);
 
     // RENDER: ResultContainer
     return (
         <div className={styles.container}>
-            <textarea className={styles.result} />
+            <textarea className={styles.result} value={resultCode} />
         </div>
     );
 };
