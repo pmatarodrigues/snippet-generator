183fdf46edfbcdcaf6bc3a4dfbd3283ce33fe50b
Pedro Mata Rodrigues <me@pmatarodrigues.com>
2021-05-31 19:36:01 +0100

    feat: add source code link

--- common/components/elements/Copyright/Copyright.js
+++ common/components/elements/Copyright/Copyright.js
@@ -8,11 +8,14 @@ const Copyright = (props) => {
     // RENDER: Copyright
     return (
         // TODO: Add icon and possibility to open in new tab
-        <a href="https://pmatarodrigues.com">
-            <div className={styles.copyright}>
+        <div className={styles.copyright}>
+            <a href="https://github.com/pmatarodrigues/snippet-generator" target="_blank">
+                Source Code
+            </a>
+            <a href="https://pmatarodrigues.com" target="_blank">
                 by Pedro Mata Rodrigues
-            </div>
-        </a>
+            </a>
+        </div>
     );
 };
 
