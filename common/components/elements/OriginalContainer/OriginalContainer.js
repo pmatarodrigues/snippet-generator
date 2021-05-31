2f4ba6f93a7e6b2aa623035d70382b2d27d4b4d5
Pedro Mata Rodrigues <me@pmatarodrigues.com>
2021-05-31 12:27:02 +0100

    fix: allow insert characters in the middle of text using keyboard shortcuts, closes #2

--- common/components/elements/OriginalContainer/OriginalContainer.js
+++ common/components/elements/OriginalContainer/OriginalContainer.js
@@ -6,6 +6,7 @@ import styles from './OriginalContainer.module.css';
 // COMPONENT: OriginalContainer
 const OriginalContainer = (props) => {
     const { code, setCode, title, setTitle, trigger, setTrigger } = useContext(CodeContext);
+    const [totalSnippets, setTotalSnippets] = useState(0)
 
     // Verify if any code was input into original container
     const didUserStartTyping = () => {
@@ -15,16 +16,46 @@ const OriginalContainer = (props) => {
         return false;
     }
 
-    const addTabCharacter = (e) => {
+    const detectKeyPress = (e) => {
         const keyCode = e.key;
-        if (keyCode === 'Tab') {
+        if (keyCode === 'Tab') {    // TAB
             e.preventDefault();
             const TAB_SIZE = 4;
             // Insert TAB element
             // FIX: Tab is only added at the end of the Code
-            setCode(code + ' '.repeat(TAB_SIZE))
+            const currentCode = insertAtCursor(' '.repeat(TAB_SIZE));
+            setCode(currentCode)
+        } else if (e.ctrlKey && keyCode === ' ') {  // CTRL + SPACE
+            e.preventDefault();
+            const currentCode = insertAtCursor('{$' + totalSnippets + ':placeholder}');
+            setCode(currentCode)
+            // Number of snippets needs to be counted to show different data
+            setTotalSnippets(totalSnippets + 1);
         }
     }
+
+    const insertAtCursor = (myValue, myField = document.activeElement) => {
+        //IE support
+        if (document.selection) {
+            myField.focus();
+            sel = document.selection.createRange();
+            sel.text = myValue;
+        }
+        //MOZILLA and others
+        else if (myField.selectionStart || myField.selectionStart == '0') {
+            var startPos = myField.selectionStart;
+            var endPos = myField.selectionEnd;
+            myField.value = myField.value.substring(0, startPos)
+                + myValue
+                + myField.value.substring(endPos, myField.value.length);
+            myField.selectionStart = startPos + myValue.length;
+            myField.selectionEnd = startPos + myValue.length;
+        } else {
+            myField.value += myValue;
+        }
+        return myField.value;
+    }
+
 
     return (
         <div className={styles.container}>
@@ -44,7 +75,7 @@ const OriginalContainer = (props) => {
             </div>
             <textarea
                 className={styles.originalInput}
-                onKeyDown={(e) => addTabCharacter(e)}
+                onKeyDown={(e) => detectKeyPress(e)}
                 value={code}
                 onChange={(e) => setCode(e.target.value)}
             />
